1a5654767b5edfae7ebb8309d3a8bbf2e44e92b2
aleksander351 <aleksander.klajderic@gmail.com>
2023-06-29 21:35:00 +0200

    Add files via upload

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,1 +1,18 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  public folder!: string;
+  adType: 'buy' | 'sell' = 'buy';
+  private activatedRoute = inject(ActivatedRoute);
+  constructor() {}
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+}
